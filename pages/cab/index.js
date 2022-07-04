73c3c4fcfe0c0fc81ae1d8eb043182a838bce215
secretDzen <turretbox@gmail.com>
2022-07-04 17:47:42 +0700

    fix: text size

--- pages/cab/index.js
+++ pages/cab/index.js
@@ -1,7 +1,7 @@
 const Cab = () => {
   return (
     <div className="flex flex-col justify-center h-screen w-full">
-      <h2 className="text-center">Login was succesful</h2>
+      <h2 className="text-center text-3xl">Login was succesful</h2>
     </div>
   );
 };
